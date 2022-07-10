c89207ca6a029ea4fced154ae888fb4a2953b291
Peter Kosarnik <nestastnikos@github.com>
2022-07-10 16:17:18 +0200

    feat: refactor and add part 2

--- day_03/Program.fs
+++ day_03/Program.fs
@@ -1,56 +1,58 @@
 ﻿open System.IO
 open System
 
-let getNextPoint direction length (x, y) =
-    match direction with
-    | 'R' -> (x+length, y)
-    | 'L' -> (x-length, y)
-    | 'U' -> (x, y+length)
-    | 'D' -> (x, y-length)
-    | _ -> raise (ArgumentException("Invalid direction"))
-
-
-let intersects x11 x12 x21 x22 =
-  let (xmin, xmax) = min x11 x12, max x11 x12
-  (xmin <= x21 && x21 <= xmax) || (xmin <= x22 && x22 <= xmax)
-
-
-let getValue a b c =
-  match a = b with
-  | true -> a
-  | false -> c
-
-
-let findIntersection (x11, y11, x12,y12, x21,y21) =
-  let x  = getValue x11 x12 x21
-  let y  = getValue y11 y12 y21
-  (x, y)
-
 let wires =
-    "full-input.txt"
-    |> File.ReadAllLines
-    |> Seq.map (fun x -> x.Split ",")
-    |> Seq.map (fun x ->
-        x
-        |> Seq.map (fun y -> (y.[0], int y.[1..]))
-        |> Seq.scan
-            (fun line (direction:char,length:int) ->
-              getNextPoint direction length line)
-            (0,0)
-        |> Seq.pairwise)
+  "full-input.txt"
+  |> File.ReadAllLines
+  |> Seq.map(fun input ->
+    input.Split ","
+    |> Seq.fold(fun (map, (x, y, d)) v ->
+        let (dir, length) = v.[0], int v.[1..]
+        let (xStep, yStep) =
+          match dir with
+          | 'R' -> (1,0)
+          | 'L' -> (-1,0)
+          | 'U' -> (0,1)
+          | 'D' -> (0,-1)
+          | _ -> raise (ArgumentException("Invalid direction"))
+        let nextLastCoord = x+(length*xStep), y+(length*yStep), d+length
 
-let wire1 = wires |> Seq.item 0
-let wire2 = wires |> Seq.item 1
+        let newMap =
+          { 1 .. length }
+          |> Seq.fold (fun acc n ->
+            let (nextX, nextY, nextDist) = x+(n*xStep), y+(n*yStep), d+n
+            let coord = (nextX, nextY)
+            match acc |> Map.containsKey coord with
+            | true ->
+              acc
+            | false ->
+              acc |> Map.add coord nextDist
+            )
+            map
+        (newMap, nextLastCoord)
+      )
+      (Map([]), (0,0,0))
+  )
+  |> Seq.map (fun (map,_) -> map)
 
-let result =
-  Seq.allPairs wire1 wire2
-  |> Seq.filter(fun (((x11,y11),(x12,y12)),((x21,y21),(x22,y22))) ->
-    (intersects x11 x12 x21 x22 || intersects x21 x22 x11 x12) &&
-    (intersects y11 y12 y21 y22 || intersects y21 y22 y11 y12))
-  |> Seq.skip 1
-  |> Seq.map(fun(((x11,y11),(x12,y12)),((x21,y21),(x22,y22))) -> x11,y11,x12,y12,x21,y21)
-  |> Seq.map(fun x -> findIntersection x)
-  |> Seq.map(fun (x,y) -> x+y)
-  |> Seq.min
+let intersections =
+  wires
+  |> Seq.map (fun x -> x |> Map.keys |> Set.ofSeq)
+  |> Set.intersectMany
 
-printfn "%A" result
+// part 1
+let minDistanceManh =
+  intersections
+  |> Set.map (fun (x,y) -> x+y)
+  |> Set.minElement
+
+// part 2
+let minDistanceBoth =
+  intersections
+  |> Set.map (fun coord ->
+    wires
+    |> Seq.map (fun e -> e |> Map.find coord)
+    |> Seq.reduce (fun a b -> a+b))
+  |> Set.minElement
+
+printfn "%d" minDistanceBoth
